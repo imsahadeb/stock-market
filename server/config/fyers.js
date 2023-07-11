6c346d38351b1fcd22e490c1e5f3e1bb6e210ca2
sahadeb <imsahadeb@gmail.com>
2023-07-11 15:55:15 +0530

    update

--- server/config/fyers.js
+++ server/config/fyers.js
@@ -1,12 +1,7 @@
+import { fyersData } from "./fyersData.js";
 import fyers from 'fyers-api-v2';
-export const fyersData = {
-    "appId": "UGY069G3IZ-100",
-    "secretId": "7DNKQGSG6K",
-    "redirectUrl": "https://api.ghontu.in/token",
-    "fyers_acess_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE2ODkwNjUzMDAsImV4cCI6MTY4OTEyMTgyMCwibmJmIjoxNjg5MDY1MzAwLCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCa3JSZFVsRkh5ejhrdVB1VFQyalRuWlBkay0wZUxzejQ0VEluRDdsQU5FVHpUZS1sWjNSYVlXa2xwc3VES3h2a2V3Zm9WY0RBdzJfZkNnZmd0ZTgzbHV0bEJDNGdCSDdfVF9xcncwQmU1WkUxTlhvMD0iLCJkaXNwbGF5X25hbWUiOiJTQUhBREVCIEJBUk1BTiIsIm9tcyI6IksxIiwiZnlfaWQiOiJEUzAyMjgzIiwiYXBwVHlwZSI6MTAwLCJwb2FfZmxhZyI6Ik4ifQ.TtxLvgVEWDFTo2cj3pAnUlNYjDr-vgftLQo9J1_bKoQ"
-  };
-  fyers.setAppId(fyersData.appId);
-  fyers.setRedirectUrl(fyersData.redirectUrl);
-  fyers.setAccessToken(fyersData.fyers_acess_token);
+fyers.setAppId(fyersData.appId);
+fyers.setRedirectUrl(fyersData.redirectUrl);
+fyers.setAccessToken(fyersData.fyers_acess_token);
 
-  export default fyers;
+export default fyers;
---
+++ server/config/fyersData.js
@@ -0,0 +1,6 @@
+export const fyersData = {
+  "appId": "UGY069G3IZ-100",
+  "secretId": "7DNKQGSG6K",
+  "redirectUrl": "https://api.ghontu.in/token",
+  "fyers_acess_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE2ODkwNzEwNzgsImV4cCI6MTY4OTEyMTgzOCwibmJmIjoxNjg5MDcxMDc4LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCa3JTM21DQlc3RUZSOHJhSkQzX3lDd1JrMnk3VzlFVnlPbnY3UEJZUEgwOHdlV0d6eEM0VmNWS1lHOFpNQkFSd3VIRUIxMGI5VWxzZEE3NnNhUXJkZFQ5TzJEV1QwMnVmQ0d5YTRVa2pJWnJsdUZTUT0iLCJkaXNwbGF5X25hbWUiOiJTQUhBREVCIEJBUk1BTiIsIm9tcyI6IksxIiwiZnlfaWQiOiJEUzAyMjgzIiwiYXBwVHlwZSI6MTAwLCJwb2FfZmxhZyI6Ik4ifQ.WRhB25rBMlgLNwjSE28yFXj41ZQ6wdqFo8zKRKUoIr4"
+};
